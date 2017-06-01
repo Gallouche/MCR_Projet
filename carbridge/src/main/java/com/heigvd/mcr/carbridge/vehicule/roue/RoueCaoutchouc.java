0f55f3be9605236948d4d6656e0669f37467b201
Michela Zucca <michela.zucca@heig-vd.ch>
2017-06-01 22:03:17 +0200

    Implémentaon des rous

--- carbridge/src/main/java/com/heigvd/mcr/carbridge/vehicule/roue/RoueCaoutchouc.java
+++ carbridge/src/main/java/com/heigvd/mcr/carbridge/vehicule/roue/RoueCaoutchouc.java
@@ -5,103 +5,77 @@ import com.heigvd.mcr.carbridge.vehicule.obstacle.Obstacle;
 /**
  * Created by User on 26.05.2017.
  *
- * RoueCaoutchouc: Sa hauteur varie, devenant petite puis grande puis petite etc...
- * avec le temps. Son frottement varie en fonction de sa hauteur.
+ * RoueCaoutchouc: Sa hauteur varie, devenant petite puis grande puis petite
+ * etc... avec le temps. Son frottement varie en fonction de sa hauteur.
  */
-public class RoueCaoutchouc implements Roue{
+public class RoueCaoutchouc implements Roue {
 
-    private int vie;
-    private boolean isPetiteRoue; // 0 => grande, 1 => peite
-    private int hauteurCmPetite;
-    private int hauteurCmGrande;
-    private double coeffPetite ;  // Coef entre 0.5 et 0.8 d'après wiki
-    private double coeffGrande;
+   private int solidite;
+   private double coeff; // 0 => grande, 1 => peite
+   private int hauteur;
+   private int VITESSE_MAX = 60;
 
-    public RoueCaoutchouc(){
-        this.vie = 100;
-        this.isPetiteRoue = false;
-        this.hauteurCmPetite = 60;
-        this.hauteurCmGrande = 120;
-        this.coeffPetite = 0.3;  // Coef entre 0.5 et 0.8 d'après wiki
-        this.coeffGrande = 0.8;
-    }
+   public RoueCaoutchouc() {
+      this.solidite = 100;
+      this.hauteur = 120;
+      this.coeff = 0.7;
+   }
 
-    /**
-     * Récupérer la hauteur d'une roue.
-     * @return la hauteur de la roue
-     */
-    @Override
-    public int getHauteur() {
-        if(isPetiteRoue){
-            return hauteurCmPetite;
-        }else{
-            return hauteurCmGrande;
-        }
-    }
+   /**
+    * Récupérer la hauteur d'une roue.
+    *
+    * @return la hauteur de la roue
+    */
+   @Override
+   public int getHauteur() {
+      return hauteur;
+   }
 
-    /**
-     * Récupérer le coefficient de frottement de la roue. Le
-     * coefficient devrait être compris dans l'intervalle [0, 1]
-     * un coefficient de 0 signifiant aucune friction et un
-     * coefficient de 1 une friction maximale.
-     * @return le coefficient de frottement de la roue
-     */
-    @Override
-    public double getCoeffFrottement() {
-        if(vie == 0) {
-            return 1.0;
-        }
+   /**
+    * Récupérer le coefficient de frottement de la roue. Le coefficient devrait
+    * être compris dans l'intervalle [0, 1] un coefficient de 0 signifiant
+    * aucune friction et un coefficient de 1 une friction maximale.
+    *
+    * @return le coefficient de frottement de la roue
+    */
+   @Override
+   public double getCoeffFrottement() {
+      if (solidite == 0) {
+         return 1.0;
+      }
 
-        if(isPetiteRoue){
-            isPetiteRoue = false; // changement d'état
-            return coeffPetite;
-        }
-        else {
-            isPetiteRoue = true; // Changement d'état
-            return coeffGrande;
-        }
-    }
+      return coeff;
+   }
 
-    /**
-     * Collision avec un obstacle.
-     * @param obstacle l'obstacle heurté
-     */
-    @Override
-    public void collision(Obstacle obstacle, double vitesse) {
-        // Si vitesse plus grande que 30km/h => collision possible
-       if(isPetiteRoue){
-            collisionPetiteRoue(obstacle,vitesse);
-       }else{
-           collisionGrandeRoue(obstacle,vitesse);
-       }
-    }
+   /**
+    * Collision avec un obstacle.
+    *
+    * @param obstacle l'obstacle heurté
+    */
+   @Override
+   public void collision(Obstacle obstacle, double vitesse) {
+      // Si vitesse plus grande que 60km/h => collision possible
+      if (vitesse > VITESSE_MAX) {
+         if (hauteur / 2 < obstacle.getHauteur()) {
+              solidite -= 10;
+              coeff += 0.05;
+              hauteur -= 10;
+         } else {
+            obstacle.destruire();
+         }
+      }
 
-    /**
-     * La solidité actuelle de la roue. Une solidité de 0 devrait
-     * signifier une roue cassée, et une roue dans cet état
-     * devrait retourner un coefficient de frottement de 1.
-     * @return la solidité de la roue.
-     */
-    @Override
-    public int getSolidite() {
-        return vie ;
-    }
+   }
 
-    private void collisionPetiteRoue(Obstacle obstacle, double vitesse){
-        if(vitesse > 30){
-            if(obstacle.getHauteur() > hauteurCmPetite/2) {
-                vie -= 50;
-            }
-        }
-    }
-
-    private void collisionGrandeRoue(Obstacle obstacle, double vitesse){
-        if(vitesse > 80){
-            if(obstacle.getHauteur() > hauteurCmGrande/2) {
-                vie -= 5;
-            }else{
-                obstacle.destruire();
-            }
-        }
-    }
+   /**
+    * La solidité actuelle de la roue. Une solidité de 0 devrait signifier une
+    * roue cassée, et une roue dans cet état devrait retourner un coefficient de
+    * frottement de 1.
+    *
+    * @return la solidité de la roue.
+    */
+   @Override
+   public int getSolidite() {
+      return solidite;
+   }
 }
--- carbridge/src/main/java/com/heigvd/mcr/carbridge/vehicule/roue/RoueFragile.java
+++ carbridge/src/main/java/com/heigvd/mcr/carbridge/vehicule/roue/RoueFragile.java
@@ -6,70 +6,75 @@ import com.heigvd.mcr.carbridge.vehicule.obstacle.Obstacle;
  * Created by User on 26.05.2017.
  *
  *
- * RoueFragile: Une roue qui frotte très peu, mais qui se fragilise avec le temps,
- * et qui perd donc des valeurs de solidité. Elle perd 50% de sa vie maximale à chaque collisions.
+ * RoueFragile: Une roue qui frotte très peu, mais qui se fragilise avec le
+ * temps, et qui perd donc des valeurs de solidité. Elle perd 50% de sa solidite
+ * maximale à chaque collisions.
  */
+public class RoueFragile implements Roue {
 
-public class RoueFragile implements Roue{
+   private int solidite;
+   private int hauteurCm;
+   private double coeff;  // Coef entre 0.5 et 0.8 d'après wiki
+   private int VITESSE_MAX;
 
-    private int vie;
-    private int hauteurCm;
-    private double coeff;  // Coef entre 0.5 et 0.8 d'après wiki
+   public RoueFragile() {
+      this.solidite = 100;
+      this.coeff = 0.2;
+      this.hauteurCm = 60;
+   }
 
-    public RoueFragile(){
-        this.vie = 100;
-        this.coeff = 0.3;
-        this.hauteurCm = 60;
-    }
+   /**
+    * Récupérer la hauteur d'une roue.
+    *
+    * @return la hauteur de la roue
+    */
+   @Override
+   public int getHauteur() {
+      return hauteurCm;
+   }
 
-    /**
-     * Récupérer la hauteur d'une roue.
-     * @return la hauteur de la roue
-     */
-    @Override
-    public int getHauteur() {
-        return hauteurCm;
-    }
+   /**
+    * Récupérer le coefficient de frottement de la roue. Le coefficient devrait
+    * être compris dans l'intervalle [0, 1] un coefficient de 0 signifiant
+    * aucune friction et un coefficient de 1 une friction maximale.
+    *
+    * @return le coefficient de frottement de la roue
+    */
+   @Override
+   public double getCoeffFrottement() {
+      if (solidite == 0) {
+         return 1.0;
+      }
 
-    /**
-     * Récupérer le coefficient de frottement de la roue. Le
-     * coefficient devrait être compris dans l'intervalle [0, 1]
-     * un coefficient de 0 signifiant aucune friction et un
-     * coefficient de 1 une friction maximale.
-     * @return le coefficient de frottement de la roue
-     */
-    @Override
-    public double getCoeffFrottement() {
-        if(vie == 0) {
-            return 1.0;
-        }
+      return coeff;
+   }
 
-        return coeff;
-    }
+   /**
+    * Collision avec un obstacle.
+    *
+    * @param obstacle l'obstacle heurté
+    */
+   @Override
+   public void collision(Obstacle obstacle, double vitesse) {
+      // Si vitesse plus grande que 30km/h => collision possible
+      if (vitesse > VITESSE_MAX && hauteurCm / 2 < obstacle.getHauteur()) {
+         // Si l'hauteur de l'obstacle dépasse la moitié de la hauteur de la roue => Collision
+         solidite -= 50;
 
-    /**
-     * Collision avec un obstacle.
-     * @param obstacle l'obstacle heurté
-     */
-    @Override
-    public void collision(Obstacle obstacle, double vitesse) {
-        // Si vitesse plus grande que 30km/h => collision possible
-        if(vitesse > 30){
-            // Si l'hauteur de l'obstacle dépasse la moitié de la hauteur de la roue => Collision
-            if(hauteurCm/2 < obstacle.getHauteur()){
-                vie -= 50;
-            }
-        }
-    }
+      } else {
+         obstacle.destruire();
+      }
+   }
 
-    /**
-     * La solidité actuelle de la roue. Une solidité de 0 devrait
-     * signifier une roue cassée, et une roue dans cet état
-     * devrait retourner un coefficient de frottement de 1.
-     * @return la solidité de la roue.
-     */
-    @Override
-    public int getSolidite() {
-       return vie ;
-    }
+   /**
+    * La solidité actuelle de la roue. Une solidité de 0 devrait signifier une
+    * roue cassée, et une roue dans cet état devrait retourner un coefficient de
+    * frottement de 1.
+    *
+    * @return la solidité de la roue.
+    */
+   @Override
+   public int getSolidite() {
+      return solidite;
+   }
 }
